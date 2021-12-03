f52c7e966532e6f6165c5f4f6b0b73caa96fd9ac
nav-28 <pwadhwa@ualberta.ca>
2021-12-02 20:55:20 -0700

    added file for day 3

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv) {
+
+    if (argc == 0){
+        printf("incorrect usage!! Please provide input file");
+    }
+    int x;
+    char y[50];
+    // read data
+
+
+
+    return 0;
+}
